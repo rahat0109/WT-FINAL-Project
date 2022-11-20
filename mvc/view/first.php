4a4c7bbe6606f96e365bc6800dbb39ee472c7b5d
Hasibul Islam <114330667+rahat0109@users.noreply.github.com>
2022-11-20 14:01:11 +0600

    Add files via upload

---
+++ mvc/view/first.php
@@ -0,0 +1,74 @@
+<html>
+  <head>
+    <title>About Us</title>
+    <link rel="stylesheet" href="style.css" />
+  </head>
+  <body>
+    <table
+      border="2"
+      align="center"
+      width="860px"
+      style="border-collapse: collapse; margin-top: 30px; margin-bottom: 50px"
+    >
+      <!-- Top Bars (start) -->
+      <tr>
+        <td>
+          <nav style="margin: 10px 10px; display: block; float: left">
+           
+            <h1>XYZ HOSPITAL</h1>
+          </nav>
+        </td>
+      </tr>
+      <tr>
+        <td>
+          <nav style="margin: 30px 20px; display: block; float: right">
+          
+            <a href="./doctorregistration.html">Registration</a> |
+            <a href="./doctorlogin.html">Log in</a> |
+          </nav>
+        </td>
+      </tr>
+     
+
+      <tr>
+        <td style="padding: 80px">
+          <!--about us-->
+          <h2 style="text-align: center">About Us!</h2>
+          <h3 style="text-align: center">
+            Welcome To XYZ HOSPITAL</span>
+          </h3>
+          <p>
+          XYZ HOSPITAL is a Professional Management System Platform. Here we will provide you only interesting content, which you will like very much. We're dedicated to providing you the best of Management System, with a focus on dependability. We're working to turn our passion for Management System into a booming online website. We hope you enjoy our Management System as much as we enjoy offering them to you.
+ Please give your support and love.
+
+Thanks For Visiting Our Site
+
+<br /><br />
+           <tr align="center">
+            <td>
+            <span
+              style="
+                color: blue;
+                font-size: 16px;
+                font-weight: bold;
+                text-align: center;
+              "
+              >Have a nice day!</span
+            >
+</tr>
+</td>
+          </p>
+       
+        </td>
+      </tr>
+
+<br /><br />
+      <tr align="center">
+        <td>
+          <nav>
+            <a href="./about.php">About Us</a>
+          </nav>
+    
+    </table>
+  </body>
+</html>
